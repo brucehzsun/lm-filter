5cc036712c13060858ddcf953cea4566af72b0f2
hongzhi.sun <hongzhi.sun@rokid.com>
2023-02-20 17:05:37 +0800

    add

--- filter_text.sh
+++ filter_text.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-python wiki_zh/wk_zh_2019_filter.py > logs/log.txt
+python wiki_zh/wk_zh_2019_filter.py
--- wiki_zh/wk_zh_2019_filter.py
+++ wiki_zh/wk_zh_2019_filter.py
@@ -3,9 +3,28 @@ import json
 import re
 
 zhPattern = re.compile(u'[\u4e00-\u9fa5]+')
-delete_list = ['《', '》', '（）', "「", "」", '）', '（', '“', '”', '(', ')', '〈', '〉', '-', '{', '}', '"', '·', '|',
+delete_list = ['《', '》', '（）', "「", "」", '）', '（', '“', '”', '(', ')', '〈', '〉', '-', '–', '{', '}', '"', '·', '|',
                ',', '‧', '[', ']', '*', '#', '%', '±', '℃', ' ', '〇', '．', '……', '=']
 replace_map = {'～': '到', '.': "", ':': '', '°': '度', '－': ' '}
+
+
+def to_lm_str(text: str):
+    english = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    output = ['<s>']
+    buffer = ''
+    for s in text:
+        if s in english or s in english.upper():  # 英文或数字
+            buffer += s
+        else:  # 中文
+            if buffer.strip():
+                output.append(buffer)
+            buffer = ''
+            if not s.strip() == '':
+                output.append(s)
+    if buffer:
+        output.append(buffer)
+    output.append('</s>')
+    return " ".join(output)
 
 
 def parse_text(text: str):
@@ -29,6 +48,8 @@ def parse_text(text: str):
             if result:
                 if (result.end() - result.start()) == 1:
                     continue
+            else:
+                continue
             # 全中文情况下去除空格
             result = re.findall(r'[a-zA-Z0-9]+', value)
             if not result:
@@ -37,14 +58,12 @@ def parse_text(text: str):
             if len(value) < 3:
                 continue
             ret.append(value)
-            print(value)
         # if v.startwith("1\")
     return ret
 
 
 def parse_wk_file(path: str, name: str):
     file_path = os.path.join(path, name)
-    print(f"file_path={file_path}")
     ret = []
     with open(file_path, 'r') as file:
         for line in file.readlines():
@@ -55,7 +74,6 @@ def parse_wk_file(path: str, name: str):
                 elif key == "url":
                     continue
                 elif key == "title":
-                    print('')
                     result = parse_text(value)
                     ret.extend(result)
                 elif key == "text":
@@ -64,35 +82,42 @@ def parse_wk_file(path: str, name: str):
                 else:
                     print(f"unknown key = {key} = > {value}")
                     exit(-1)
+    print(f"{name}.size={len(ret)}")
     return ret
 
     # with open(file_path) as file:
     #     while()
 
 
-def writer_file(result: list, dir_name: str):
-    path = os.path.join("data", dir_name + ".txt")
-    with open(path, 'w') as f:
-        for text in result:
-            result = ['<s>']
-            for char in text:
-                result.append(char)
-            result.append('</s>')
-            f.write(" ".join(result).strip() + "\n")
+def process_dir(path: str, dir_name: str, corpus: str):
+    count = 0
+    out_path = os.path.join("data", corpus, dir_name + ".txt")
+    raw_path = os.path.join("raw_data", corpus, dir_name + ".txt")
+    with open(out_path, 'w') as f, open(raw_path, 'w') as raw_f:
+        for file_name in os.listdir(os.path.join(path, dir_name)):
+            result = parse_wk_file(os.path.join(path, dir_name), file_name)
+            count += len(result)
+            for text in result:
+                raw_f.write(text + "\n")
+                ret = to_lm_str(text)
+                f.write(ret + "\n")
+    print(f"dir={dir_name},size={count}")
+    return count
 
 
-def process_text():
-    path = "/Users/brucesun/asr-corpus/lm/wiki_zh"
+def process_corpus(dir_path: str, corpus: str):
+    path = os.path.join(dir_path, corpus)
+    count = 0
     for dir_name in os.listdir(path):
         if not dir_name.startswith("."):
-            for file_name in os.listdir(os.path.join(path, dir_name)):
-                result = parse_wk_file(os.path.join(path, dir_name), file_name)
-                writer_file(result, dir_name)
-                break
+            count += process_dir(path, dir_name, corpus)
             break
+    print(f"{corpus} finish,count={count} >>>>>>>>>>>>>>")
 
 
 if __name__ == '__main__':
-    process_text()
+    path = "/Users/brucesun/asr-corpus/lm"
+    corpus = 'wiki_zh'
+    process_corpus(path, corpus)
     # text = ["爱我中华", '中华人民共和国']
     # writer_file(text, 'test')

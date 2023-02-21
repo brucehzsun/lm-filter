1424dddd3893fa5f4bb0e474a4bf0529be49af97
hongzhi.sun <hongzhi.sun@rokid.com>
2023-02-21 15:13:15 +0800

    增加小写数字转大写数字。ASR输出的全是大写数字

--- filter_text.sh
+++ filter_text.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
+export PYTHONPATH=${pwd}:$PYTHONPATH
 python wiki_zh/wk_zh_2019_filter.py
--- utils/number_util.py
+++ utils/number_util.py
@@ -2,13 +2,14 @@ import cn2an
 import re
 from wiki_zh import wk_zh_2019_filter as wk
 
-NUM_DICT = {'0': '零', '1': '一', '2': '二', '3': '三', '4': '四', '5': '五', '6': '六', '7': '七', '8': '八', '9': '九'}
+NUM_DICT = {'0': '零', '1': '一', '2': '二', '3': '三', '4': '四', '5': '五', '6': '六', '7': '七', '8': '八', '9': '九',
+            '０': '零', '１': '一', '２': '二', '３': '三', '４': '四', '５': '五', '６': '六', '７': '七', '８': '八', '９': '九'}
 
 pattern_date = re.compile(r'\d{1,4}(年|月|日|小时|分钟|秒)')  # 查找日期时间数字
 pattern_number = re.compile(r'\d+')
 pattern_phone = re.compile(
     r'((\d{11})|((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})))')  # 查找电话号码
-number = '0123456789'
+number = '0123456789１２３４５６７８９０'
 
 
 def convert_normal_number(text: str):
@@ -39,16 +40,23 @@ def convert_data_number(text: str):
     while ret is not None:
         for i in range(ret.span()[0], ret.span()[1]):
             if letters[i] in number:
-                letters[i] = NUM_DICT[text[i]]
+                letters[i] = NUM_DICT[letters[i]]
         ret = pattern_date.search(''.join(letters))
     return "".join(letters)
 
 
 def convert_number(text: str):
+    print(f"before convert_number={text}")
     text = convert_data_number(text)
+    print(f"after convert_number={text}")
     text = convert_phone_number(text)
+    print(f"after convert_number={text}")
     text = convert_normal_number(text)
+    print(f"after convert_number={text}")
     return text
 
 
-
+if __name__ == '__main__':
+    text = '筑后史谈会　昭和１０年刊の复制'
+    text = convert_data_number(text)
+    print(text)

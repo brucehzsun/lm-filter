f702c485e1bea537f1735eff1a51f807c53f19a4
hongzhi.sun <hongzhi.sun@rokid.com>
2023-02-22 17:43:01 +0800

    支持在linux上运行

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 ## news_2016_cn 语料
 
 #### news2016zh_valid
+
 语料数：
+
 #### news2016zh_train
+
 语料数：
+
+nohup ./filter_text.sh > logs/log.txt 2>&1 &
--- filter_text.sh
+++ filter_text.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-export PYTHONPATH=$pwd:$PYTHONPATH
+export PYTHONPATH=`pwd`:$PYTHONPATH
 python src/new_zh/nwes_zh_filter.py
 python src/wiki_zh/wk_zh_2019_filter.py
 python src/wiki_zh/merger.py

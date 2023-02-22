b2ef72ce43139bd354817b165d1372e126d2692b
hongzhi.sun <hongzhi.sun@rokid.com>
2023-02-22 21:00:17 +0800

    增加参数传递，避免修改代码

--- filter_wiki_linux.sh
+++ filter_wiki_linux.sh
@@ -3,5 +3,5 @@
 export PYTHONPATH=$(pwd):$PYTHONPATH
 CORPUS_PATH=/home/bruce/asr/data
 nohup python3 src/lm/wk_zh_2019_filter.py --data_path $CORPUS_PATH >logs/wiki.log 2>&1 &
-nohup python3 src/lm/nwes_zh_filter.py --data_path $CORPUS_PATH >logs/news.log 2>&1 &
+nohup python3 src/lm/news_zh_filter.py --data_path $CORPUS_PATH >logs/news.log 2>&1 &
 nohup python3 src/lm/baike_qa_filter.py --data_path $CORPUS_PATH >logs/baike.log 2>&1 &

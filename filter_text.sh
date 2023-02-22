ad8144f09e985cf88a4a52b19bcd225a98748181
hongzhi.sun <hongzhi.sun@rokid.com>
2023-02-22 18:10:58 +0800

    支持在linux上运行

--- filter_text.sh
+++ filter_text.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 export PYTHONPATH=`pwd`:$PYTHONPATH
-python src/new_zh/nwes_zh_filter.py
-python src/wiki_zh/wk_zh_2019_filter.py
-python src/wiki_zh/merger.py
-python src/baike_qa/baike_qa_filter.py
+python3 src/new_zh/nwes_zh_filter.py
+python3 src/wiki_zh/wk_zh_2019_filter.py
+python3 src/wiki_zh/merger.py
+python3 src/baike_qa/baike_qa_filter.py

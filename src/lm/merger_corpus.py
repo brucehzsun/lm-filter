66672d52424691760193638acad7aa99d6ec75c1
hongzhi.sun <hongzhi.sun@rokid.com>
2023-02-23 11:24:06 +0800

    merger corpus,去除重复元素

--- src/lm/merger_corpus.py
+++ src/lm/merger_corpus.py
@@ -1,7 +1,5 @@
-import argparse
 import os
 import datetime
-import hashlib
 import time
 
 
@@ -9,8 +7,6 @@ def merger_wik(file_paths: list):
     print(f"start merge multi file to one file...")
     corpus_path = os.path.join('data', 'rokid_lm.txt')
     total_size = 0
-    filter_count = 0
-    str_map = {}
     with open(corpus_path, 'w') as writer:
         for file_path in file_paths:
             with open(file_path, 'r') as f:
@@ -18,20 +14,15 @@ def merger_wik(file_paths: list):
                 start_time = time.time()
                 line = f.readline()  # 读取第一行
                 while line is not None and line != '':
-                    md5_val = hashlib.md5(line.encode('utf8')).hexdigest()
-                    if str_map.get(md5_val):
-                        filter_count += 1
-                    else:
-                        str_map[md5_val] = True
-                        writer.write(line)
-                        count += 1
-                        total_size += 1
-                        line = f.readline()  # 读取下一行
-                        if count % 10000 == 0:
-                            print(f"processed:{count},time={(time.time() - start_time) * 1000}ms")
-                print(
-                    f"writer,path= {file_path}, size={count},total_size={total_size},filter_count={filter_count},time={(time.time() - start_time) * 1000}ms")
-    print(f"finish, total size = {total_size},filter_count={filter_count},time={datetime.datetime.now()}")
+                    writer.write(line)
+                    count += 1
+                    total_size += 1
+                    line = f.readline()  # 读取下一行
+                    if count % 10 == 0:
+                        print(f"processed:{count},time={(time.time() - start_time) * 1000}ms")
+                used_time = (time.time() - start_time) * 1000
+                print(f"writer,path= {file_path}, size={count},total_size={total_size},time={used_time}ms")
+    print(f"finish, total size = {total_size},time={datetime.datetime.now()}")
 
 
 if __name__ == '__main__':

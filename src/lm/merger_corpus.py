4f7e4ee478f351e37faedfc45f40ea16081b35c2
hongzhi.sun <hongzhi.sun@rokid.com>
2023-02-23 10:32:48 +0800

    merger corpus

--- src/lm/merger_corpus.py
+++ src/lm/merger_corpus.py
@@ -6,24 +6,23 @@ import datetime
 def merger_wik(file_paths: list):
     print(f"start merge multi file to one file...")
     corpus_path = os.path.join('data', 'rokid_lm.txt')
-    count = 0
+    total_size = 0
     with open(corpus_path, 'w') as writer:
         for file_path in file_paths:
             with open(file_path, 'r') as f:
-                lines = f.readlines()
-                writer.writelines(lines)
-                print(f"writer,path= {file_path}, size={len(lines)},time={datetime.datetime.now()}")
-                count += len(lines)
-    print(f"finish, total size = {count},time={datetime.datetime.now()}")
+                count = 0
+                line = f.readline()  # 读取第一行
+                while line is not None and line != '':
+                    writer.write(line)
+                    count += 1
+                    total_size += 1
+                    line = f.readline()  # 读取下一行
+                print(f"writer,path= {file_path}, size={count},total_size={total_size},time={datetime.datetime.now()}")
+    print(f"finish, total size = {total_size},time={datetime.datetime.now()}")
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='merge your lm corpus')
-    parser.add_argument('--data_path', required=True, type=str, help='path of lm corpus')
-    args = parser.parse_args()
-
-    path = args.data_path
-    print(f"start merger corpus process,path={path},time={datetime.datetime.now()}")
+    print(f"start merger corpus process,time={datetime.datetime.now()}")
 
     file_paths = []
     file_paths.append(os.path.join('data', 'baike_qa2019', 'baike_qa_valid.json.txt'))

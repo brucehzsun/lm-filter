874c413ea4a35a10bd38aaf4c9be16558b48138b
hongzhi.sun <hongzhi.sun@rokid.com>
2023-03-22 16:37:06 +0800

    改用WeTextProcessor处理文本

--- src/test/test.py
+++ src/test/test.py
@@ -3,7 +3,7 @@ import re
 
 if __name__ == '__main__':
     corpus_list = ['啊第二节课SECOND CLASS', 'hello word', '面积147km²', 'NINETY FIVE TIMES FROM CLASSES',
-                   '然后 AFTER 表示在什么什么之后 AFTER CLASS 的意思就是课下放学后']
+                   '然后AFTER表示在什么什么之后AFTER CLASS的意思就是课下放学后']
     for corpus in corpus_list:
         text = text_filter.convert_to_lm_text(corpus)
         print(text)

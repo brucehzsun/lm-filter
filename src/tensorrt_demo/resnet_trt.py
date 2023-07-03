97c730fdaba2f61fc8736a6f9bb8f733e30dd5ba
hongzhi.sun <hongzhi.sun@rokid.com>
2023-07-03 19:26:40 +0800

    整理代码

--- src/tensorrt_demo/resnet_trt.py
+++ src/tensorrt_demo/resnet_trt.py
@@ -22,44 +22,56 @@ target_dtype = np.float16 if USE_FP16 else np.float32
 def preprocess_image(img):
     norm = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     result = norm(torch.from_numpy(img).transpose(0, 2).transpose(1, 2))
-    return np.array(result, dtype=np.float16)
+    return np.array(result, dtype=target_dtype)
 
 
-if USE_FP16:
-    engine_path = "/home/hzsun/python_dev/lm-filter/src/tensorrt_demo/resnet_engine_pytorch_fp16.trt"
-else:
-    engine_path = "/home/hzsun/python_dev/lm-filter/src/tensorrt_demo/resnet_engine_pytorch.trt"
+def load_egine():
+    if USE_FP16:
+        engine_path = "/home/hzsun/python_dev/lm-filter/src/tensorrt_demo/resnet_engine_pytorch_fp16.trt"
+    else:
+        engine_path = "/home/hzsun/python_dev/lm-filter/src/tensorrt_demo/resnet_engine_pytorch.trt"
 
-print(f"load model={engine_path}")
-with open(engine_path, "rb") as f:
-    engine_data = f.read()
-    runtime = trt.Runtime(trt.Logger(trt.Logger.WARNING))
-    engine = runtime.deserialize_cuda_engine(engine_data)
-
-url = 'https://images.dog.ceo/breeds/retriever-golden/n02099601_3004.jpg'
-img = resize(io.imread(url), (224, 224))
-input_batch = np.array(np.repeat(np.expand_dims(np.array(img, dtype=np.float32), axis=0), BATCH_SIZE, axis=0), dtype=np.float32)
-
-print(f"input_batch.shape={input_batch.shape}")
-print(f"input_batch.nbytes={input_batch.nbytes}")
-preprocessed_images = np.array([preprocess_image(image) for image in input_batch])
-print(f"preprocessed_images.shape={preprocessed_images.shape}")
-
-context = engine.create_execution_context()
-# need to set input and output precisions to FP16 to fully enable it
-output = np.empty([BATCH_SIZE, 1000], dtype=target_dtype)
-# allocate device memory
-d_input = cuda.mem_alloc(1 * input_batch.nbytes)
-d_output = cuda.mem_alloc(1 * output.nbytes)
-
-bindings = [int(d_input), int(d_output)]
-
-stream = cuda.Stream()
+    print(f"load model={engine_path}")
+    with open(engine_path, "rb") as f:
+        engine_data = f.read()
+        runtime = trt.Runtime(trt.Logger(trt.Logger.WARNING))
+        engine = runtime.deserialize_cuda_engine(engine_data)
+    return engine
 
 
-def predict(batch):  # result gets copied into output
-    # transfer input data to device
-    cuda.memcpy_htod_async(d_input, batch, stream)
+def get_batch_imgs():
+    url = 'https://images.dog.ceo/breeds/retriever-golden/n02099601_3004.jpg'
+    img = io.imread(url)
+    print(f"img_1.shape={img.shape}")
+    img = resize(img, (224, 224))
+    print(f"img_2.shape={img.shape}")
+    img = np.array(img, dtype=target_dtype)
+    print(f"img_3.shape={img.shape}")
+    img = np.expand_dims(img, axis=0)
+    print(f"img_4.shape={img.shape}")
+    img = np.repeat(img, BATCH_SIZE, axis=0)
+    print(f"img_5.shape={img.shape}")
+    input_batch = np.array(img, dtype=target_dtype)
+    print(f"img_6.shape={img.shape}")
+    return input_batch
+
+
+def allocate_buffers(input_batch, output):
+    # need to set input and output precisions to FP16 to fully enable it
+    # allocate device memory
+    d_input = cuda.mem_alloc(1 * input_batch.nbytes)
+    d_output = cuda.mem_alloc(1 * output.nbytes)
+
+    bindings = [int(d_input), int(d_output)]
+    return d_input, d_output, bindings
+
+
+def predict(d_input, d_output, bindings, output, preprocessed_images):
+    stream = cuda.Stream()
+    cuda.memcpy_htod_async(d_input, preprocessed_images, stream)
+
+    print(f"Warming up...")
+    # pred = predict(preprocessed_images, d_input, output, d_output, stream)
     # execute model
     context.execute_async_v2(bindings, stream.handle, None)
     # transfer predictions back
@@ -69,10 +81,22 @@ def predict(batch):  # result gets copied into output
     return output
 
 
-for i in range(2):
-    print(f"Warming up...,i={i}")
+if __name__ == "__main__":
+    engine = load_egine()
+    input_batch = get_batch_imgs()
+    output = np.empty([BATCH_SIZE, 1000], dtype=target_dtype)
+    print(f"input_batch.shape={input_batch.shape},nbytes={input_batch.nbytes}")
+    preprocessed_images = np.array([preprocess_image(image) for image in input_batch])
+    print(f"preprocessed_images.shape={preprocessed_images.shape}")
+
+    context = engine.create_execution_context()
+
+    d_input, d_output, bindings = allocate_buffers(input_batch, output)
+
+    # transfer input data to device
     start_time = time.time()
-    pred = predict(preprocessed_images)
+    pred = predict(d_input, d_output, bindings, output, preprocessed_images)
+
     indices = (-pred[0]).argsort()[:5]
-    ret = list(zip(indices, pred[0][indices]))
+    ret = list(zip(indices, output[0][indices]))
     print(f"Done warming up!,time={(time.time() - start_time) * 1000},ret={ret}")
